23978d3adcce8f8dc4edf852c2e88248113b2fab
Christian Harwood <charwood217@west-mec.org>
2021-02-23 15:25:43 -0700

    $$$

--- models/personReport.js
+++ models/personReport.js
@@ -1,4 +1,27 @@
 var mongoose =require('mongoose');
 
+const person = new mongoose.Schema({
+    Firstname: {
+        type: String,
+        required: true
+    },
+    Middlename: {
+        type: String,
+        required: false
+    },
+    Lastname: {
+        type: String,
+        required: true
+    },
+    SSN: {
+        type: Number,
+        required: true 
+    },
+    DLN: {
+        type: Number,
+        required: true
+    },
+    Gender: [{ type: Schema.Types.ObjectId, ref: 'Gender' }]
+});
 //Export Model
 module.exports = mongoose.model('personReport', personReportSchema);
