f9fb346017721015d3fd97ee7f72cf501ba0e8da
Christian Harwood <charwood217@west-mec.org>
2021-03-05 15:17:53 -0700

    Worked on controllers page

--- controllers/personReportController.js
+++ controllers/personReportController.js
@@ -1,8 +1,4 @@
-var home = require('../models/home');
 var async = require('async');
-var incident = require('../models/incident');
-var person = require('../models/person');
-var vehicele = require('../models/vehicle');
 var assert = require('assert');
 var express = require('express');
 var mongo = require('mongodb');
@@ -15,6 +11,10 @@ const { body,validationResult } = require('express-validator');
 //Populate Hom3
 exports.person_Report_Populate = function (req, res){
     res.render('personReport');
+};
+
+exports.getData = function(req, res, next){
+   
 };
 
 
@@ -33,31 +33,31 @@ exports.person_Report_Populate = function (req, res){
 //         });
 //     });
 // });
-exports.getData = function(req, res, next){
-    var resultArray = [];
-    var pull = db.RMS.person.find({});
-    pull.forEach(function(doc, err){
-        resultArray.push(doc)
-    }, function(){
-        res.render('personReport', {items: resultArray});
-    });
-};
+// exports.getData = function(req, res, next){
+//     var resultArray = [];
+//     var pull = db.RMS.person.find({});
+//     pull.forEach(function(doc, err){
+//         resultArray.push(doc)
+//     }, function(){
+//         res.render('personReport', {items: resultArray});
+//     });
+// };
 
-//Inserts data into the mongo Data Base
-router.post('/person', function(req, res, next){
-    var item = {
-        first_name: req.body.first_name,
-        last_name: req.body.last_name
+// //Inserts data into the mongo Data Base
+// router.post('/person', function(req, res, next){
+//     var item = {
+//         first_name: req.body.first_name,
+//         last_name: req.body.last_name
 
-    };
-    mongo.connect(url, function(err, db){
-        assert.equal(null, err);
-        db.collection('person').insertOne(item, function(err, result){
-            assert.equal(null, err);
-            console.log('Item Inserted');
-            db.close();
-        })
-    })
+//     };
+//     mongo.connect(url, function(err, db){
+//         assert.equal(null, err);
+//         db.collection('person').insertOne(item, function(err, result){
+//             assert.equal(null, err);
+//             console.log('Item Inserted');
+//             db.close();
+//         })
+//     })
 
-    res.redirect('/')
-});
+//     res.redirect('/')
+// });
