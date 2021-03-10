c80c01bf975c568654da784d4f7a4d2a93660246
Jeremiah Ruiz <jruiz181@west-mec.org>
2021-03-10 15:24:57 -0700

    added stuff to pages

--- controllers/dist/homeController.dev.js
+++ controllers/dist/homeController.dev.js
@@ -1,12 +1,22 @@
+"use strict";
+
 var home = require('../models/home');
+
 var async = require('async');
+
 var incident = require('../models/incident');
+
 var person = require('../models/person');
+
 var vehicele = require('../models/vehicle');
 
-const { body, validationResult } = require('express-validator');
+var _require = require('express-validator'),
+    body = _require.body,
+    validationResult = _require.validationResult; //Populate Hom3
 
-//Populate Hom3
+
 exports.home_Populate = function (req, res) {
-    res.render('home', { title: 'Home Page' });
+  res.render('home', {
+    title: 'Home Page'
+  });
 };
