48b12a54e55facc857f708fa3a28ad9cfd04aab1
Elijah Collins <ecollins152@west-mec.org>
2021-03-10 13:43:11 -0700

    fixed pug file for incident report just so the form matches others

--- public/login.js
+++ public/login.js
@@ -1,9 +1,11 @@
+// allows the validation function to be ran using the enter key, rather than pressing the button every time.
 function handleEnter(e) {
     if(e.keyCode === 13){
         validate()
     }
   }
-
+// checks both the username and password fields to see if it has the acceptable content. If it does, you can enter the rest of
+// the website, if you don't it asks you to check your information again, and doesn't let you in.
 function validate(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
